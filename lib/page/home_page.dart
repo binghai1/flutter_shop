6e601214a068c59752e6c8e7592a2b2c7205af30
lh <liuhai@liuhaideMacBook-Air.local>
2019-03-02 22:51:19 +0800

    topnavigation complete

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../services/servicesUtil.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class HomePage extends StatefulWidget {
 
   _HomePageState createState() => _HomePageState();
@@ -33,9 +34,11 @@ class _HomePageState extends State<HomePage> {
               print("111$snapshot");
               var data=json.decode(snapshot.data.toString());
               List<Map> swiperList=(data['data']['slides'] as List).cast();
+              List<Map> navigatorList =(data['data']['category'] as List).cast();
                print("66$swiperList");
               return Column(children: <Widget>[
-                SwiperComponent(swiperList:swiperList)
+                SwiperComponent(swiperList:swiperList),
+                 TopNavigator(navigatorList:navigatorList),
               ],);
             }else{
               return Center(
@@ -44,7 +47,7 @@ class _HomePageState extends State<HomePage> {
             }
           },
         ),
-
+        
     );
   }
   
@@ -57,7 +60,8 @@ class SwiperComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 333.0,
+      height: ScreenUtil.getInstance().setHeight(333),
+      width: ScreenUtil.getInstance().setWidth(750),
       child: Swiper(
         itemBuilder:(BuildContext context,int index){
           return Image.network(swiperList[index]['image'],fit:BoxFit.fill);
@@ -66,6 +70,39 @@ class SwiperComponent extends StatelessWidget {
         pagination:SwiperPagination(),
         autoplay: true,
       ),
+    );
+  }
+}
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  Widget __gridViewItemUI(BuildContext context,item){
+      return InkWell(
+        onTap: (){
+          print("点击了小部件");
+        },
+        child: Column(
+          children: <Widget>[
+            Image.network(item['image'],width: ScreenUtil.getInstance().setWidth(95)),
+            Text(item['mallCategoryName'])
+          ],
+        ),
+      );
+  }
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    if(navigatorList.length>10){
+      navigatorList.removeRange(10, 11);
+    }
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding:EdgeInsets.all(3.0),
+      child:GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return __gridViewItemUI(context,item);
+        }).toList()),
     );
   }
 }
